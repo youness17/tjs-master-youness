1751543cb01b6df2d5c77c48a0ca4e8e5e17ff3a
youness <youness.snoussi17@gmail.com>
2020-09-07 10:49:25 +0200

    youness

---
+++ Snoussi_Youness_test_javascript/public/js/main.js
@@ -0,0 +1,94 @@
+// 2
+
+let maBomaolen = ""
+
+// 3
+
+let monArray = []
+
+// 4
+
+let monObjet = {
+    nome: "snoussi"
+}
+
+// 5
+
+let monString = 'youness'
+
+// 6
+
+let = nombre(25)
+
+// 7
+
+let monObjet = {
+
+    nome: "snoussi",
+    prenom: "youness",
+
+}
+nomeComplet = function() {
+    return nome + " " + prenom;
+  };
+
+  console.log(nomeComplet);
+
+// //8
+
+function N (nome,prenom);
+
+// 9
+
+let  monInt = "monInt"
+if (monInt<8) {
+    console.log("oui");
+}
+
+else{
+    console.log("non");
+}
+
+// 10
+
+ let num = Number(10)
+ for (let i = 0; i < num; i++) {
+    console.log(i);
+ }
+
+//  11
+
+let date = new Date();
+
+console.log(date);
+
+// 12
+
+let chaine = {
+    nom: "bonjour",
+
+    function() {
+        return this.nom.toUpperCase
+    }
+}
+console.log(chaine.nom);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
